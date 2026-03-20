711bf8f15551ee07b477f81d97790f30bfef1e35
ysmandza <ysmandza@slac.stanford.edu>
2026-03-20 12:58:54 -0700

    Create RELEASE_SITE symlink relative to workspace

--- ci/setup_ci_env.sh
+++ ci/setup_ci_env.sh
@@ -1,3 +1,5 @@
-ln -sf /cds/group/pcds/epics/${BASE_MODULE_VERSION}/modules/RELEASE_SITE ${WORKSPACE}/RELEASE_SITE
+# Create RELEASE_SITE symlink relative to workspace
+ln -sf /cds/group/pcds/epics/${BASE_MODULE_VERSION}/modules/RELEASE_SITE \
+    ${WORKSPACE}/../RELEASE_SITE
 
 echo "[setup_ci_env] Linked RELEASE_SITE to ${WORKSPACE}/RELEASE_SITE"
